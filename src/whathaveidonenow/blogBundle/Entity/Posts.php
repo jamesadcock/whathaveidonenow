0ca2d872cd57da3aee80f03701f5d4be0c5de7b5
jamesa <jamesadcock1980@gmail.com>
2013-07-28 09:26:10 +0100

    db set-up

---
+++ src/whathaveidonenow/blogBundle/Entity/Posts.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace whathaveidonenow\blogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="posts")
+ */
+
+
+class Posts
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+     protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $comment;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $path;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Posts
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     * @return Posts
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string 
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Posts
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+}
