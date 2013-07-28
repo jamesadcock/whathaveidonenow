28f288bd226f7886d9dd665cd4d9c422587bc1c5
jamesa <jamesadcock1980@gmail.com>
2013-07-28 20:11:57 +0100

    persists to db

--- src/whathaveidonenow/blogBundle/Controller/DefaultController.php
+++ src/whathaveidonenow/blogBundle/Controller/DefaultController.php
@@ -3,13 +3,35 @@
 namespace whathaveidonenow\blogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use whathaveidonenow\blogBundle\Entity\Posts;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         //return new Response('<html><body> Blog Test!</body> </html>');
+
+        $posts = $this->getDoctrine()
+            ->getRepository('whathaveidonenowblogBundle:Posts')
+            ->findAll();
+
+        $data=['posts'=> $posts];
+
         return $this->render(
-            'whathaveidonenowblogBundle:Default:index.html.twig');
+            'whathaveidonenowblogBundle:Default:index.html.twig',$data);
+    }
+
+    public function postAction()
+    {
+        $post = new Posts();
+        $post->setPath('/web/020733_2_1.jpg');
+        $post->setComment('This is a comment I have written');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+
+        return new Response('Created product id '.$post->getId());
     }
 }
--- src/whathaveidonenow/blogBundle/Entity/Posts.php
+++ src/whathaveidonenow/blogBundle/Entity/Posts.php
@@ -22,11 +22,7 @@ class Posts
     /**
      * @ORM\Column(type="string", length=100)
      */
-     protected $name;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
     protected $comment;
 
     /**
@@ -42,29 +38,6 @@ class Posts
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     * @return Posts
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string 
-     */
-    public function getName()
-    {
-        return $this->name;
     }
 
     /**
